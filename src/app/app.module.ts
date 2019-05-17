c0127357e94850a5d67befa7b0ed291110afb0f7
Mamadou Lo COLY <momocoly42@gmail.com>
2019-05-17 16:34:45 +0000

    Gestion formulaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MyNavComponent } from './my-nav/my-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatDividerModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatNativeDateModule, MAT_DATE_LOCALE, MatSelectModule } from '@angular/material';
 import { HomeComponent } from './components/home/home.component';
 import { CarteComponent } from './components/carte/carte.component';
 import { ReserveComponent } from './components/reserve/reserve.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { MainDashComponent } from './main-dash/main-dash.component';
 import { DishTableComponent } from './dish-table/dish-table.component';
+
+import { QuickLunchService } from './services/quick-lunch.service'
 
 @NgModule({
   declarations: [
@@ -40,9 +43,17 @@ import { DishTableComponent } from './dish-table/dish-table.component';
     MatMenuModule,
     MatTableModule,
     MatPaginatorModule,
-    MatSortModule
+    MatSortModule,
+    MatDividerModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
-  providers: [],
+  providers: [{provide: MAT_DATE_LOCALE, useValue: 'fr-FR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/main-dash/main-dash.component.ts
+++ src/app/main-dash/main-dash.component.ts
@@ -1,33 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { QuickLunchService } from '../services/quick-lunch.service';
+import { Food } from '../models/food.interface'
 
 @Component({
   selector: 'main-dash',
   templateUrl: './main-dash.component.html',
   styleUrls: ['./main-dash.component.css']
 })
-export class MainDashComponent {
+export class MainDashComponent implements OnInit{
+
+  constructor(private breakpointObserver: BreakpointObserver, private qls: QuickLunchService) {}
+  
+  burgers: Food[];
+  pizzas: Food[];
+  galettes: Food[];
+
+  ngOnInit() {
+    this.burgers = this.qls.getBurger();
+    this.pizzas = this.qls.getPizzas();
+    this.galettes = this.qls.getGalettes();
+  }
+
   /** Based on the screen size, switch from standard to one column per row */
   cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
     map(({ matches }) => {
       if (matches) {
         return [
-          { title: 'A la carte', cols: 2, rows: 1 },
-          { title: 'Burgers', cols: 2, rows: 1 },
-          { title: 'Galettes/Crêpes', cols: 2, rows: 2 },
-          { title: 'Pizzas', cols: 2, rows: 1 }
+          { title: 'Burgers', cols: 2, rows: 1, id: 'brg' },
+          { title: 'Galettes/Crêpes', cols: 2, rows: 2, id: 'glt' },
+          { title: 'Pizzas', cols: 2, rows: 1, id: 'pzz' }
         ];
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 1, rows: 1 },
-        { title: 'Card 3', cols: 1, rows: 2 },
-        { title: 'Card 4', cols: 1, rows: 1 }
+          { title: 'Burgers', cols: 2, rows: 1, id: 'brg' },
+          { title: 'Galettes/Crêpes', cols: 2, rows: 2, id: 'glt' },
+          { title: 'Pizzas', cols: 2, rows: 1, id: 'pzz' }
       ];
     })
   );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
 }
